1d129b1440082aadaeda3fc07933be37cb15469e
jcrochon <jcrochon@uic.edu>
2016-04-10 21:54:45 -0500

    first commit

---
+++ 2_4/src/main.c
@@ -0,0 +1,16 @@
+/*
+ * 2.4.c
+ *
+ *  Created on: Apr 10, 2016
+ *      Author: jcrochon
+ */
+
+/* helloworld program */
+
+#include <stdio.h>
+
+int main(void) {
+	printf("Hello, World!\n");
+
+	return 0;
+}
