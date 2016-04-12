4a2bec4e62dca92e54efa764251669e6a5a7187d
jcrochon <jcrochon@achilles.cc.uic.edu>
2016-04-12 12:48:39 -0500

    # WARNING: head commit changed in the meantime
    adding new3 project

--- 5_0/src/main.c
+++ 5_0/src/main.c
@@ -4,19 +4,19 @@
  *  Created on: Apr 12, 2016
  *      Author: jcrochon
  */
-#include <stdlib.h>
-#include <stdio.h>
+#include "../src/main.h"
 
 // prototypes
 int plus_one(int);
 
 int main(void) {
-	int i = 10, j;
+	int i = 10, j, a = 5, b = 10;
 
 	j = plus_one(i); /* THE CALL */
 
 	printf("i + 1 is %d\n", j);
 
+	doit(a, b);
 	return 0;
 }
 
@@ -24,3 +24,4 @@ int plus_one(int n) /* THE DEFINITION */
 {
 	return n + 1;
 }
+
--- 5_0/src/main.h
+++ 5_0/src/main.h
@@ -8,6 +8,16 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+#include <stdlib.h>
+#include <stdio.h>
 
+void doit(int a, int b) {
+	printf("\nplus_one(a):          %3i", plus_one(a)); /* the type of a is int */
+	printf("\nplus_one(10):         %3i", plus_one(10)); /* the type of 10 is int */
+	printf("\nplus_one(1 + 10):     %3i", plus_one(1 + 10)); /* the type of the whole expression is still int */
+	printf("\nplus_one(a + 10):     %3i", plus_one(a + 10)); /* the type of the whole expression is still int */
+	printf("\nplus_one(a + b):      %3i", plus_one(a + b)); /* the type of the whole expression is still int */
+	printf("\nplus_one(plus_one(a): %3i", plus_one(plus_one(a))); /* oooo! return value is int, so it's ok!  */
+}
 
 #endif /* MAIN_H_ */
