fa89bf362009de9d2208bc1691e2c89696559940
Jason C. Rochon <jcrochon@cpan.org>
2016-04-12 14:14:42 -0500

    new3 file updated

--- new3/src/main.c
+++ new3/src/main.c
@@ -4,7 +4,16 @@
  *  Created on: Apr 12, 2016
  *      Author: jcrochon
  */
+#include <datetimeapi.h>
 
 void main(void) {
-	;
+	time_t rawtime;
+	struct tm * timeinfo;
+
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+
+	printf("I printed something today!");
+
+	printf("Current local time and date: %s", asctime(timeinfo));
 }
