cec434f37c8bb48b1955d07a0423336e1ce569c2
jcrochon <jcrochon@achilles.cc.uic.edu>
2016-04-12 04:04:29 -0500

    changed 5.0's main function add global .gitignore

--- 5_0/src/main.c
+++ 5_0/src/main.c
@@ -7,7 +7,20 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// prototypes
+int plus_one(int);
 
 int main(void) {
+	int i = 10, j;
+
+	j = plus_one(i); /* THE CALL */
+
+	printf("i + 1 is %d\n", j);
+
 	return 0;
+}
+
+int plus_one(int n) /* THE DEFINITION */
+{
+	return n + 1;
 }
