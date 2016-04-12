5e3242e4a24ac85918be97a7471a2bbcff1d2e3a
jcrochon <jcrochon@achilles.cc.uic.edu>
2016-04-12 08:10:28 -0500

    added new project

---
+++ new2/src/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: Apr 12, 2016
+ *      Author: jcrochon
+ */
+
+#include "../src/jcr.h"
+
+int main(void) {
+	printf("\nStarting thankYou...\n");
+	thankYou();
+	return 0;
+}
